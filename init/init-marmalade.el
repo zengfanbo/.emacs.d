4b4a4de39e6acf5f877c1d844b19d837dbe0298e
zengfanbo <zengfanbo2009@gmail.com>
2013-12-01 22:28:16 +0800

    change some style

--- init/init-marmalade.el
+++ init/init-marmalade.el
@@ -1,10 +1,14 @@
 (add-to-list 'load-path "~/.emacs.d/init")
 (require 'package)
-;;(add-to-list 'package-archives '("marmalade" ."http://marmalade-repo.org/packages/"))
+(setq  package-archives '(
+				 ("gnu" . "http://elpa.gnu.org/packages/")
+				 ("marmalade" . "http://marmalade-repo.org/packages/")
+				 ("melpa" . "http://melpa.milkbox.net/packages/"))
+)
 
 (package-initialize)
 
-(require 'tabbar-autoloads)
-(require 'init-color-theme)
+;;(require 'tabbar-autoloads)
+;;(require 'init-color-theme)
 
 (provide 'init-marmalade)
